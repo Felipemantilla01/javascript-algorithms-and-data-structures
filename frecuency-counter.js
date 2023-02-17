6f366c75bd221fa1fd3e63348b8d7681e509702a
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2023-02-17 16:01:01 -0500

    is anagram frecuency counter example

--- frecuency-counter.js
+++ frecuency-counter.js
@@ -35,6 +35,7 @@ function same(array1, array2) {
   // return true
 }
 
+// the algorithm complexity is O(n^2) because we have a foreach and inside an indexOf
 function same(array1, array2) {
   // verify the length of both arrays, if different return true
   if (array1.length !== array2.length) {
@@ -56,13 +57,14 @@ function same(array1, array2) {
   return true;
 }
 
+console.log("Is Same Testing");
 console.log(same([1, 2, 3], [1, 4, 9]));
 console.log(same([1, 2, 3], [1, 4, 9, 9]));
 console.log(same([1, 2, 3, 3], [1, 4, 9, 9]));
 console.log(same([], []));
 
 /**
- * Using frecuency counter patter
+ * Using frecuency counter patter, in this way the algorithm complexity is reduced to O(3n) -> O(n)
  */
 
 function same(array1, array2) {
@@ -88,3 +90,95 @@ function same(array1, array2) {
   }
   return true;
 }
+
+/**
+ * Example: Anagram challenge: Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase,
+ * or name formed by rearraging the letters of another, such as cinema, formed from cinema.
+ */
+
+/**
+ * 1. understanding the problem
+ *  -   re-write: create a function to check if two strings have the same number and frecuency characters, must return true or false
+ *  -   what are the inputs: two strings
+ *  -   what are the outputs: a boolean value
+ */
+
+/**
+ * 2. Explore concrete example
+ *  -   simple examples
+ * isAnagram("cinema","iceman") : true
+ * isAnagram("cinema","icemanw") : false
+ *  -   more complex examples
+ * isAnagram("aazr","zraa") : true
+ * isAnagram("aazrrraax","zraarraxa") : true
+ *  -   void inputs
+ * isAnagram("","") : true
+ *  -   invalid inputs
+ * isAnagram(332,123) : false
+ * isAnagram(321,123) : false
+ */
+
+/**
+ * 3. Break it down
+ *  -   Create a pseudo code with comments writing everything the code should do, as specific as you can do it
+ */
+
+function isAnagram(str1, str2) {
+  // validate inputs, check if both are strings
+  // both must be have the same length
+  // convert both strings to mapped objects and count the frecuency of each char
+  // iterate on the first mapped string object
+  // if the char dont exist in the second mapped sttring return false
+  // if the char frecuecy on the first mapped object is different to the second one return false
+  // return true
+}
+
+/**
+ * 4. Simplify
+ *  -   If you have any difficulty, avoid it by the moment and simplify until the end, then come back and solve it
+ */
+
+function isAnagram(str1, str2) {
+  // validate inputs, check if both are strings
+  if (typeof str1 !== "string" || typeof str2 !== "string") {
+    return false;
+  }
+  // both must be have the same length
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  // convert both strings to mapped objects and count the frecuency of each char
+  let frecuencyCounter1 = {};
+  let frecuencyCounter2 = {};
+  for (let char of str1) {
+    frecuencyCounter1[char] = (frecuencyCounter1[char] || 0) + 1;
+  }
+  for (let char of str2) {
+    frecuencyCounter2[char] = (frecuencyCounter2[char] || 0) + 1;
+  }
+  // iterate on the first mapped string object
+  for (let key in frecuencyCounter1) {
+    // if the char dont exist in the second mapped sttring return false
+    if (!frecuencyCounter2[key]) {
+      return false;
+    }
+    // if the char frecuecy on the first mapped object is different to the second one return false
+    if (frecuencyCounter1[key] != frecuencyCounter2[key]) {
+      return false;
+    }
+  }
+  // return true
+  return true;
+}
+
+/**
+ * 5. Lookback and refactor
+ */
+
+// testing
+console.log("Is Anagram Testing");
+console.log(isAnagram("", ""));
+console.log(isAnagram("aasssdsds", "asdaddsds"));
+console.log(isAnagram("cinema", "iceman"));
+console.log(isAnagram("aazrrraax", "zraarraxa"));
+console.log(isAnagram(321, 123));
