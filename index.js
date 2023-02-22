6239fbacf4a2a086e8fb7db12fa80391db0eb1a6
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2023-02-22 16:53:53 -0500

    naive search strings

--- index.js
+++ index.js
@@ -64,3 +64,21 @@ function binarySearch(arr, item) {
   }
   return arr[middle] === item ? middle : -1;
 }
+
+// naiveSearch string
+
+function naiveSearch(long, short) {
+  var count = 0;
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+console.log(
+  "naiveSearch:",
+  naiveSearch("asdq qwe a waasd lolsq dq lol das", "lol")
+);
