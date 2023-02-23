03e518328826f1a78ddd736a907509a98c798392
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2023-02-23 07:02:23 -0500

    selection sort example

--- index.js
+++ index.js
@@ -15,4 +15,37 @@ function bubbleSort(arr) {
   return arr;
 }
 
-console.log("bubbleSort", bubbleSort([5, 98, 7, 5, 6, 8, 12, 2, 56]));
+// console.log("bubbleSort", bubbleSort([5, 98, 7, 5, 6, 8, 12, 2, 56]));
+
+function selectionSort(array) {
+  let tempMinIndex = 0;
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start < end) {
+    for (let index = start; index < array.length - 1; index++) {
+      if (array[index] <= array[index + 1]) {
+        if (array[index] < array[tempMinIndex]) {
+          tempMinIndex = index;
+        }
+      } else {
+        if (array[index + 1] < array[tempMinIndex]) {
+          tempMinIndex = index + 1;
+        }
+      }
+    }
+    // after find the tempIndex, we need to swap it
+    if (array[start] !== array[tempMinIndex]) {
+      [array[start], array[tempMinIndex]] = [array[tempMinIndex], array[start]];
+    }
+    start += 1;
+    tempMinIndex = start;
+  }
+
+  return array;
+}
+
+console.log(
+  "selectionSort",
+  selectionSort([5, 98, 1, 7, 5, 3, 32, 201, 6, 8, 12, 2, 56])
+);
--- readme.md
+++ readme.md
@@ -2,6 +2,8 @@
 
 # Sorting Algorithms
 
+https://visualgo.net/
+
 What is sorting?
 
 - Sorting is the process of rearranging the items in a collection (e.g. an array) so that the items are in some kind of order
@@ -9,3 +11,7 @@ What is sorting?
 ## Bubble Sort
 
 The bubble sort is based on comparisons on each position of the array and its next, like arr[i] < arr[i+1], and swapping the numbers until no swaps happens.
+
+## Selection Sort
+
+Similar to Bubble sort, but insted of first placing large values into sorted position, it places small values into sorted position
