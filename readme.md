5c04f9c8159024d40b28a8bcff1c70f7b5cf99ab
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2023-03-01 10:11:15 -0500

    basic structure and push method

--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 # JavaScript Algorithms and Data Structures Course
+
+# Data Structures
+
+## Linked Single List
+
+It is a data structure that contains a `head`, `tail` and `length` property.
+Linked list consist of nodes, and each node has a value and a pointer to another node or null
---
+++ singly-linked-list.js
@@ -0,0 +1,32 @@
+// node
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+const singlyLinkedList = new SinglyLinkedList();
+console.log(singlyLinkedList.push(45));
+console.log(singlyLinkedList.push(32));
