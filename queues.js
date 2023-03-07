8edf16f683b12346cb75a764d73d10280e7a8ee3
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2023-03-07 10:43:06 -0500

    adding Queue class and its methods, some tests

---
+++ queues.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  enqueue(value) {
+    let newNode = new Node(value);
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    return ++this.size;
+  }
+
+  dequeue() {
+    if (!this.first) return null;
+
+    let temp = this.first;
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return temp.value;
+  }
+}
+
+let queue = new Queue();
+
+console.log(queue.enqueue("first"));
+console.log(queue.enqueue("second"));
+console.log(queue.enqueue("third"));
+
+console.log(queue);
+
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+
+console.log(queue);
--- readme.md
+++ readme.md
@@ -6,3 +6,12 @@
 
 It is a data structure that contains a `head`, `tail` and `length` property.
 Linked list consist of nodes, and each node has a value and a pointer to another node or null
+
+
+## Stacks 
+
+Its a Data structure,  LIFO (Last in First Out), we can create this using arrays, but will have unnecessary indexing, so its a good idea implementing like a singly linked list.
+
+## Queues 
+
+Its a Data Structure, FIFO (First in First Out), we can create this using arrays, but will have unnecessary indexing,
