4c37759f692df2e7e5fafa162c4bf91656610a01
Felipe Mantilla <andres.mantilla@gorillalogic.com>
2023-03-13 13:59:10 -0500

    implementation for has tables example

---
+++ hash-tables.js
@@ -0,0 +1,39 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  // the hash function
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.mmin(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, map]);
+  }
+
+  get(key) {
+    let index = this._hash(key);
+
+    if (this.keyMap[index]) {
+      for (let i = 0; i < globalThis.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          return this.keyMap[index][i][1];
+        }
+      }
+    }
+
+    return undefined;
+  }
+}
